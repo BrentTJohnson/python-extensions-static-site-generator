d12bdd03346bbeabe01a37fc7b31fca6500836a7
Brent Johnson <33670213+BrentTJohnson@users.noreply.github.com>
2020-07-30 16:04:47 -0400

    Stats event created
    placed hooks for the various stat events. Finished the extension project.

---
+++ ssg/extensions/stats.py
@@ -0,0 +1,24 @@
+import time
+from ssg import hooks
+
+
+start_time = None
+
+total_written = 0
+
+@hooks.register("start_build")
+def start_build():
+    global start_time
+    start_time = time.localtime()
+
+@hooks.register("written")
+def written():
+    global total_written
+    total_written += 1
+
+@hooks.register("stats")
+def stats():
+    final_time = time.localtime() - start_time
+    average = final_time // total_written if total_written != 0
+    report = f"Converted: {total_written} · Time: {final_time:.2f} sec · Avg: {average:.4f} sec/file"
+    return report
